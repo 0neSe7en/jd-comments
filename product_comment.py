c1b2532c6fbf6bae9c2653f5f27c31b95786190c
0neSe7en <siyuan.se7en@gmail.com>
2016-03-07 23:01:19 +0800

    fix bug.

--- product_comment.py
+++ product_comment.py
@@ -16,83 +16,105 @@ product_col = db['products']
 tag_col = db['tags']
 user_col = db['users']
 comment_col = db['comments']
-page = 0
 
 user_agents_file = open('uas.txt', mode='r')
 ua_list = [x.strip() for x in user_agents_file.readlines()]
 
 
-def fetch_comments(skuid):
-    global page
-    page = 0
-    while True:
-        sec = random.randint(1, 3)
-        time.sleep(sec)
-        ua = random.choice(ua_list)
-        url = default['comment_host'] % (skuid, page)
-        print('Start fetch:', url)
-        comments_json = requests.get(url, headers={'User-Agent': ua})
-        print('Fetch Done:', url)
-        if not comments_json.text:
-            break
-        comments = json.loads(comments_json.text)
-        if len(comments['comments']):
-            save_to_mongo(skuid, comments, page)
-            page += 1
-            print('INFO: Progress Sku=%s, Comment Page=%d' % (skuid, page))
+class Spider:
+    def __init__(self, skuid):
+        self.skuid = skuid
+        self.retry = 0
+        self.page = 0
+        self.host = default['comment_host']
+        self.changed = False
+
+    def do_retry(self):
+        if self.retry > 3 and self.changed:
+            return False
+        if self.changed:
+            self.retry += 1
         else:
-            print('INFO: Finish Sku=%s' % skuid)
-            break
+            if self.host == default['comment_host']:
+                self.host = default['another_host']
+            else:
+                self.host = default['comment_host']
+            self.changed = True
+        return True
 
+    def clear_retry(self):
+        self.retry = 0
+        self.changed = False
 
-def save_to_mongo(skuid, comments, page):
-    print('Save to mongo')
-    if page == 0:
-        summary = comments['productCommentSummary']
-        save_to_summary = {
-            'productId': summary.get('productId'),
-            'scoreCount': [
-                summary.get('score1Count'),
-                summary.get('score2Count'),
-                summary.get('score3Count'),
-                summary.get('score4Count'),
-                summary.get('score5Count'),
-            ],
-            'commentCount': summary.get('commentCount'),
-            'rates': {
-                'good': summary.get('goodRate'),
-                'general': summary.get('general'),
-                'poor': summary.get('poor')
+    def fetch_comments(self):
+        while True:
+            sec = random.randint(1, 3)
+            time.sleep(sec)
+            ua = random.choice(ua_list)
+            url = self.host % (self.skuid, self.page)
+            print('Start fetch:', url)
+            comments_json = requests.get(url, headers={'User-Agent': ua})
+            print('fetch Done:', url)
+            if not comments_json.text:
+                if self.do_retry():
+                    continue
+                else:
+                    break
+            comments = json.loads(comments_json.text)
+            if len(comments['comments']):
+                self.clear_retry()
+                self.save_to_mange(comments)
+                self.page += 1
+                print('INFO: Progress Sku=%s, Comment Page=%d' % (self.skuid, self.page))
+            else:
+                print('INFO: SKU=%s finished, Page=%d' % (self.skuid, self.page))
+                break
+
+    def save_to_mange(self, comments):
+        print('Save to mongo')
+        if self.page == 0:
+            summary = comments['productCommentSummary']
+            save_to_summary = {
+                'productId': summary.get('productId'),
+                'scoreCount': [
+                    summary.get('score1Count'),
+                    summary.get('score2Count'),
+                    summary.get('score3Count'),
+                    summary.get('score4Count'),
+                    summary.get('score5Count'),
+                ],
+                'commentCount': summary.get('commentCount'),
+                'rates': {
+                    'good': summary.get('goodRate'),
+                    'general': summary.get('general'),
+                    'poor': summary.get('poor')
+                }
             }
-        }
-        tags = comments['hotCommentTagStatistics']
-        print('Update product')
-        product_col.update_one({'skuid': skuid}, {'$set': save_to_summary}, upsert=True)
-        print('Start update tags')
-        for tag in tags:
-            tag['tag_id'] = tag['id']
-            del tag['id']
-            tag_col.update_one({'tag_id': tag['tag_id']}, {'$set': tag}, upsert=True)
+            tags = comments['hotCommentTagStatistics']
+            product_col.update_one({'skuid': self.skuid}, {'$set': save_to_summary}, upsert=True)
+            for tag in tags:
+                tag['tag_id'] = tag['id']
+                del tag['id']
+                tag_col.update_one({'tag_id': tag['tag_id']}, {'$set': tag}, upsert=True)
 
-    for comment in comments['comments']:
-        save_to_user = {
-            'nickname': comment.get('nickname'),
-            'levelId': comment.get('userLevelId'),
-            'province': comment.get('userProvince'),
-            'registerTime': datetime.strptime(comment.get('userRegisterTime'), '%Y-%m-%d %H:%M:%S'),
-            'levelName': comment.get('userLevelName')
-        }
-        if comment.get('anonymousFlag') == 1:
-            print('Update users by regex')
-            user_col.insert_one(save_to_user)
-        else:
-            print('Update users by nickname directly')
-            user_col.update_one({
-                'nickname': save_to_user['nickname'],
-                'registerTime': save_to_user['registerTime']
-            }, {'$set': save_to_user}, upsert=True)
-        exists = comment_col.find_one({'id': comment['id']})
-        if not exists:
+        for comment in comments['comments']:
+            exists = comment_col.find_one({'id': comment['id']})
+            if exists:
+                continue
+            save_to_user = {
+                'nickname': comment.get('nickname'),
+                'levelId': comment.get('userLevelId'),
+                'province': comment.get('userProvince'),
+                'registerTime': datetime.strptime(comment.get('userRegisterTime'), '%Y-%m-%d %H:%M:%S'),
+                'levelName': comment.get('userLevelName')
+            }
+            if comment.get('anonymousFlag') == 1:
+                user_exists = user_col.find_one({'registerTime': save_to_user['registerTime']})
+                if not user_exists:
+                    user_col.insert_one(save_to_user)
+            else:
+                user_col.update_one({'registerTime': save_to_user['registerTime']}, {'$set': save_to_user}, upsert=True)
+            comment['skuid'] = self.skuid
             print('Insert', comment['id'])
             comment_col.insert_one(comment)
 
@@ -100,15 +122,15 @@ def save_to_mongo(skuid, comments, page):
 while True:
     sku = r.spop('comment_list').decode('utf-8')
     print('SKUID:', sku)
+    spider = Spider(sku)
     try:
-        fetch_comments(sku)
+        spider.fetch_comments()
     except KeyboardInterrupt:
         print('Keyboard')
-        r.hset('progress', sku, page)
+        r.hset('progress', spider.skuid, spider.page)
         break
     except Exception as e:
         print('Someting happen', e)
         traceback.print_exc()
-        # r.hset('progress', sku, page)
         time.sleep(180)
         continue
