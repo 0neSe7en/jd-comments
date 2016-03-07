b53c1a9975b37c46b30d04a0f31ff1f7f3acf372
0neSe7en <siyuan.se7en@gmail.com>
2016-03-07 13:28:54 +0800

    fix bugs for handle exception.

--- product_info.py
+++ product_info.py
@@ -19,6 +19,10 @@ product_col = db['products']
 
 
 def fetch_product(skuid):
+    exists = product_col.find_one({'skuid': skuid})
+    if exists and exists.get('商品名称'):
+        print('JUMP:', skuid)
+        return
     product_info = requests.get(host % skuid).text
     soup = BeautifulSoup(product_info, 'lxml')
     summary = soup.find_all('li', title=True)
@@ -31,17 +35,20 @@ def fetch_product(skuid):
     for attr in attrs:
         attrs_desc[attr.string] = attr.parent.contents[1].string
     summary_desc.update(attrs_desc)
-    product_col.update_one({'skuid': skuid}, {'$set': summary_desc}, upsert=True)
+    try:
+        product_col.update_one({'skuid': skuid}, {'$set': summary_desc}, upsert=True)
+    except Exception:
+        print(Exception)
 
 
 while True:
-    sku = r.srandmember('product_list').decode('utf-8')
+    sku = r.spop('product_list').decode('utf-8')
     print('SKUID:', sku)
     try:
         fetch_product(sku)
     except KeyboardInterrupt:
         print('Keyboard')
-        r.hset('progress', sku, page)
+        r.set('product_progress:' + sku, '1')
         break
     except Exception as e:
         print('Someting happen', e)
